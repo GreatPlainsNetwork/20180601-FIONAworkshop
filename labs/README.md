f095be7866354464453ecdc29d0502feee383ad2
George B. Robb III <robbg@missouri.edu>
2018-05-20 21:08:29 -0500

    cleanups and logistics update on juypter-lab startup engine

--- labs/README.md
+++ labs/README.md
@@ -3,3 +3,5 @@
 Collection of labs in the format of juypter notebooks, playbooks, raw code, or puppet manifests.
 
 Labs placed in here should be participant facing and ready to be executed.
+
+./PRP-Montery/ is a colleciton of labs from the 2018 PRP-Fiona workshop in Montery California.
--- misc/start-juypter.sh
+++ misc/start-juypter.sh
@@ -3,4 +3,5 @@ echo "crack open firewall..."
 firewall-cmd --add-port=8888/tcp
 
 echo "load up juypter lab"
-jupyter-lab --no-browser --allow-root --ip=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+#jupyter-lab --no-browser --allow-root --ip=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+jupyter-lab --no-browser --allow-root --ip=$(hostname -f)
